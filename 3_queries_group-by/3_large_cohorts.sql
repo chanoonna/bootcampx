bec9445e314505fda53604228fcde6fc5e01b676
chanoonna <chanoonna@gmail.com>
2021-02-02 00:23:29 +0000

    add: queries with group by, order by and having

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS cohort_name, COUNT(students.*) AS student_count
+FROM cohorts
+JOIN students
+ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT(students.*) > 17
+ORDER BY COUNT(students.*);
